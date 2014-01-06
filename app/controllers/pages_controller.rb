95ea551de178e8a2f9c72e4a22b992b9a7021fc2
girish <girish@ubuntu.(none)>
2014-01-06 14:27:11 +0530

    added site layout

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,28 +1,37 @@
 class PagesController < ApplicationController
   def home
+  @title="Home"
   end
 
   def vision
+  @title="Vision"
   end
 
   def trail
+  @title="trail"
   end
 
   def transport
+  @title="transport"
   end
 
   def accommadition
+  @title="accommadition"
   end
 
   def activities
+  @title="activities"
   end
 
   def rates
+  @title="rates"
   end
 
   def reserve
+  @title="reserve"
   end
 
   def contact
+  @title="contact"
   end
 end
